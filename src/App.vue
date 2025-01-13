8796eb7470fc422c9969a78387eeaa600d0e39bf
SyukranDev <m.syukransoleh@amast.com.my>
2025-01-13 15:56:50 +0800

    crate home components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
   import Navbar from '@/components/Navbar.vue';
+  import Hero from '@/components/Hero.vue';
+  import HomeCards from '@/components/HomeCards.vue';
 </script>
 
 <template>
    <Navbar />
+   <Hero title="Test Title" subtitle="Hello" />
+   <HomeCards />
 </template>
 
